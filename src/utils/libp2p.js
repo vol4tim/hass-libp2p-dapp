7d310c8cb9a84883f5801a193c3bedd9a8293919
vol4tim <vol4tim@gmail.com>
2023-10-30 13:59:28 +0400

    add circuit relay transport

--- src/utils/libp2p.js
+++ src/utils/libp2p.js
@@ -4,6 +4,7 @@ import { webSockets } from "@libp2p/websockets";
 import * as filters from "@libp2p/websockets/filters";
 import { pipe } from "it-pipe";
 import { createLibp2p } from "libp2p";
+import { circuitRelayTransport } from "libp2p/circuit-relay";
 import { identifyService } from "libp2p/identify";
 import { fromString as uint8ArrayFromString } from "uint8arrays/from-string";
 import { toString as uint8ArrayToString } from "uint8arrays/to-string";
@@ -13,7 +14,8 @@ export const createNode = async () => {
     transports: [
       webSockets({
         filter: filters.all
-      })
+      }),
+      circuitRelayTransport()
     ],
     streamMuxers: [mplex()],
     connectionEncryption: [noise()],
