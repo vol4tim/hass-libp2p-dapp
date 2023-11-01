bdf96f93229f057379c87e46f6df77641ca27df3
vol4tim <vol4tim@gmail.com>
2023-11-01 15:55:35 +0400

    add webrtc

--- src/utils/libp2p.js
+++ src/utils/libp2p.js
@@ -1,5 +1,7 @@
 import { noise } from "@chainsafe/libp2p-noise";
+import { yamux } from "@chainsafe/libp2p-yamux";
 import { mplex } from "@libp2p/mplex";
+import { webRTC } from "@libp2p/webrtc";
 import { webSockets } from "@libp2p/websockets";
 import * as filters from "@libp2p/websockets/filters";
 import { pipe } from "it-pipe";
@@ -11,13 +13,19 @@ import { toString as uint8ArrayToString } from "uint8arrays/to-string";
 
 export const createNode = async () => {
   const node = await createLibp2p({
+    addresses: {
+      listen: ["/webrtc"]
+    },
     transports: [
       webSockets({
         filter: filters.all
       }),
-      circuitRelayTransport()
+      webRTC(),
+      circuitRelayTransport({
+        discoverRelays: 1
+      })
     ],
-    streamMuxers: [mplex()],
+    streamMuxers: [yamux(), mplex()],
     connectionEncryption: [noise()],
     services: {
       identify: identifyService()
@@ -32,10 +40,15 @@ export const createNode = async () => {
 };
 
 export async function request(connection, topic, data) {
+  console.log(1);
   if (connection.status !== "open") {
     return;
   }
-  const stream = await connection.newStream([topic]);
+  console.log(2);
+  const stream = await connection.newStream([topic], {
+    // runOnTransientConnection: true
+  });
+  console.log(3);
   return pipe(
     [uint8ArrayFromString(JSON.stringify(data))],
     stream,
